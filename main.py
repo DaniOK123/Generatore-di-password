a1ddbb9228859a8c879b6809664ef227e7384cfb
DaniOK123 <daniellopresti@icloud.com>
2026-06-28 22:43:50 +0200

    iniziata la prima parte

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-print("Hello")
+import random
+
+password = ********
+
+def menu():
+    print("Benvenuti al generatore di password!!!")
+
+
+def main():
+
+    while True:
+        
+        try:
+            scelta = int(input("Vuoi creare una nuova password?\n Yes => 1\n No => 2"))
+
+        except ValueError:
+            print("Devi scegliere un numero tra 1 e 2!")
+
+
+
+def cambio():
+    pass
+
+
+
+
+def run():
+    menu()
+    main()
+
+
+if __name__ == '__main__':
+    run()

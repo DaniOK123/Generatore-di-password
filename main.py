c25bfb3d8f17ee074c3d26489c14a5ead5fcdb2a
DaniOK123 <daniellopresti@icloud.com>
2026-06-29 00:57:44 +0200

    Terminato progetto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
+import string 
 
-password = ********
 
 def menu():
     print("Benvenuti al generatore di password!!!")
@@ -11,15 +11,29 @@ def main():
     while True:
         
         try:
-            scelta = int(input("Vuoi creare una nuova password?\n Yes => 1\n No => 2"))
+            scelta = int(input("Vuoi creare una nuova password?\n Yes => 1\n No => 2\n Scegli => "))
+            print("----------------")
 
         except ValueError:
             print("Devi scegliere un numero tra 1 e 2!")
+            continue
+
+
+        if scelta == 1:
+            password = stringa_random()
+            print(f"La tua nuova password è: {password}")
+            break 
+            
+        elif scelta == 2:
+            print("Grazie per averci scelto!")
+            break
 
 
 
-def cambio():
-    pass
+def stringa_random(lunghezza=10):
+
+    caratteri = string.ascii_letters + string.digits  # a-z, A-Z, 0-9
+    return ''.join(random.choice(caratteri) for _ in range(lunghezza))
 
 
 
